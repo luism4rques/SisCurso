98c6db6ce341226d36a9c651fed3e0a6f9da341c
Luis Marques <luis.marques@whitefox.com.br>
2021-03-28 15:11:36 -0300

    Criacao do Formulario

--- src/WebUI/Controllers/ContatoController.cs
+++ src/WebUI/Controllers/ContatoController.cs
@@ -1,10 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using WebUI.Models;
 
 namespace WebUI.Controllers
 {
     public class ContatoController : Controller
     {
         public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contato contato)
         {
             return View();
         }
