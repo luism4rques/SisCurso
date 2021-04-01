19786e71d62b11bff86e2dc3d814e602e6d90c73
Luis Marques <luis.marques@whitefox.com.br>
2021-04-01 10:54:01 -0300

    Salvando

---
+++ src/WebUI/Models/ContatoViewModel.cs
@@ -0,0 +1,10 @@
+namespace WebUI.Models
+{
+    public class ContatoViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Email { get; set; }
+    }
+}
