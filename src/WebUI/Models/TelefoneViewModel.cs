8dc5eebc90d427ddfc3eb7a57f8e5467930eb328
Luis Marques <luis.marques@whitefox.com.br>
2021-04-02 17:50:24 -0300

    Salvando

--- src/WebUI/Models/TelefoneViewModel.cs
+++ src/WebUI/Models/TelefoneViewModel.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace WebUI.Models
 {
     public class TelefoneViewModel
     {
         public int Id { get; set; }
         public int ContatoId { get; set; }
+
+        [Required(ErrorMessage = "O 'Telefone' deve ser preenchido.")]
+        [MinLength(9, ErrorMessage = "O 'Telefone' deve ter no mínimo 9 caracteres.")]
+        [MaxLength(100, ErrorMessage = "O 'Telefone' deve ter no máximo 20 caracteres.")]
+        [DisplayName("Telefone")]
         public string Numero { get; set; }
     }
 }
