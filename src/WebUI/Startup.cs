3e5883eccf684d179b0d4d04701c691b1b193ccd
Luis Marques <luis.marques@whitefox.com.br>
2021-04-18 13:36:54 -0300

    Salvando

--- src/WebUI/Startup.cs
+++ src/WebUI/Startup.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using DAL;
+using DAO;
 using DTO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
