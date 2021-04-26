c93197dbe402d5a4f892066ea9de585fe0c75ddb
Luis Marques <luis.marques@whitefox.com.br>
2021-04-26 13:29:26 -0300

    Salvando

--- src/WebUI/Startup.cs
+++ src/WebUI/Startup.cs
@@ -18,12 +18,14 @@ namespace WebUI
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            CurrentEnvironment = env;
         }
 
         public IConfiguration Configuration { get; }
+        private IWebHostEnvironment CurrentEnvironment{ get; set; } 
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -32,7 +34,7 @@ namespace WebUI
             
             services.AddAutoMapper();
 
-            services.AddDependencyInjection();
+            services.AddDependencyInjection(CurrentEnvironment);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- src/WebUI/StartupConfigure/DependencyInjectionConfigure.cs
+++ src/WebUI/StartupConfigure/DependencyInjectionConfigure.cs
@@ -1,15 +1,24 @@
 using DAO;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace WebUI.StartupConfigure
 {
     public static class DependencyInjectionConfigure
     {
-        public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
+        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, IWebHostEnvironment env)
         {
-            services.AddTransient<IContatoDAO, DAO.SQLServer.ContatoDAO>();
-            services.AddTransient<ITelefoneDAO, DAO.SQLServer.TelefoneDAO>();
-
+            if(env.IsDevelopment())
+            {
+                services.AddTransient<IContatoDAO, DAO.SQLServer.ContatoDAO>();
+                services.AddTransient<ITelefoneDAO, DAO.SQLServer.TelefoneDAO>();
+            }
+            else
+            {
+                services.AddTransient<IContatoDAO, DAO.SQLite.ContatoDAO>();
+                services.AddTransient<ITelefoneDAO, DAO.SQLite.TelefoneDAO>();
+            }
             return services;
         }
     }
